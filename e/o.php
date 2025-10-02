6aeef4b66256b7422ee14ef0d0d5e04079c10c51
newtestformeo2 <150899291+newtestformeo2@users.noreply.github.com>
2025-10-02 09:02:25 +0500

    Create o.php

---
+++ e/o.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$length = "высокий";
+echo "Мой дом " . $length . "<br>";
+echo "Этот мужчина " . $LENGTH . "<br>";
+echo "Тот столб " . $lenGTH . "<br>";
+?>
+
+</body>
+</html>
